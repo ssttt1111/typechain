0968ce3488f7e2cf0f87b5cb697c19d591e6d5d4
Suwan Choi <ssttt1111@gmail.com>
2019-04-15 18:37:05 +0900

    #11 Validating Block Structure part Two

--- src/index.ts
+++ src/index.ts
@@ -67,14 +67,32 @@ const createNewBlock = (data: string): Block => {
   return newBlock;
 };
 
+const getHashforBlock = (aBlock: Block): string =>
+  Block.claculateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
+
 const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
   if (!Block.validateStructure(candidateBlock)) {
     return false;
-  } else if(previousBlock.index + 1 !== candidateBlock.index){
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
     return false;
-  } else if(previousBlock.hash !== candidateBlock.previousHash) {
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
     return false;
-  } else if()
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatesBlock())) {
+    blockchain.push(candidateBlock);
+  }
 };
 
 export {};
